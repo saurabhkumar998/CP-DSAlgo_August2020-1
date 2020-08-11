bb18abb44779011e7a8ce5f9924fc24e8ec524bf
sachin235 <sachin23hisar@gmail.com>
2020-08-11 20:36:24 +0530

    Add pointers

---
+++ Day1/pointers.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+	int x = 10;
+	char c = 'A';
+
+	int *ptr = &x;
+	char*ptr2 = &c;
+
+	cout<<x<<endl;
+	cout<<c<<endl;
+	cout<<ptr<<endl;
+	cout<<ptr2<<endl;
+
+	// cout<<sizeof(x)<<endl;
+	// cout<<sizeof(c) <<endl;
+	// cout<<sizeof(ptr)<<endl;
+	// cout<<sizeof(ptr2)<<endl;
+
+	// cout<< ptr <<endl;
+	// cout<< (void *)ptr2 <<endl;
+	// Explict Typecasting to void* so that cout treats char address as address
+	// cout<<sizeof(void *)<<endl;
+
+
+	void* p = &x;
+	cout<< p <<endl;
+
+	p = &c;
+	cout<< p <<endl;
+
+	return 0;
+}
---
+++ Day1/reference_variable.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	int a= 10;
+	//Reference Variable
+	int &b = a;  
+	b = b + 1;
+
+	cout<<a <<endl; // 11
+	cout<< b  <<endl; //11
+
+	a = 20;
+	cout<<a<<" "<<b<<endl;
+
+	return 0;
+}
