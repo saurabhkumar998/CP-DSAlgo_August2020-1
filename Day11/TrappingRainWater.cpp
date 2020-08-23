37585f9df7078b7b2bc767e5441d3b306e57270c
sachin235 <sachin23hisar@gmail.com>
2020-08-23 19:33:26 +0530

    DP Intro

---
+++ Day11/TrappingRainWater.cpp
@@ -0,0 +1,34 @@
+// Leetcode 42. Trapping Rain Water
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if(n<=2)
+            return 0;
+        int left[n];
+        int right[n];
+        
+        left[0] = height[0];
+        for(int i=1; i<n; i++){
+            left[i] = max(left[i-1], height[i]);
+        }
+        
+        right[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--){
+            right[i] = max(right[i+1], height[i]);
+        }
+        
+        for(int i=0; i<n; i++){
+            left[i] = min(left[i], right[i]);
+        }
+        
+        int ans=0;
+        for(int i=0; i<n; i++){
+            left[i] -= height[i];
+            ans += left[i];
+        }
+        
+        return ans;
+    }
+};
